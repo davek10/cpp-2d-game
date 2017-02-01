76e70f27eb075de64553eee0a0b785f2074fcac9
Robert Wörlund <worlund@kth.se>
2017-02-01 16:35:42 +0100

    tested keyboard functions for an object

--- testGaem/testGaem/Source.cpp
+++ testGaem/testGaem/Source.cpp
@@ -1,4 +1,8 @@
 #include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+using namespace std;
 int main() {
 
 	sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
@@ -16,8 +20,50 @@ int main() {
 				window.close();
 		}
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		{
+			sf::Vector2f pos = shape.getPosition();
+			float newX = pos.x;
+			if (pos.x - 1 >= 0) {
+				newX--;
+			}
+			shape.setPosition(newX, pos.y);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			sf::Vector2f pos = shape.getPosition();
+			float newX = pos.x;
+			if (pos.x + 1.0 <= (640.0-shape.getRadius()*2)) {
+				newX++;
+			}
+			shape.setPosition(newX, pos.y);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			sf::Vector2f pos = shape.getPosition();
+			float newY = pos.y;
+			if (pos.y - 1 >= 0) {
+				newY--;
+			}
+			shape.setPosition(pos.x, newY);
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			// left key is pressed: move our character
+			sf::Vector2f pos = shape.getPosition();
+			float newY = pos.y;
+			if (pos.y + 1 <= (480-shape.getRadius()*2)) {
+				newY++;
+			}
+			shape.setPosition(pos.x, newY);
+		}
+
 		window.clear();
 		window.draw(shape);
+		window.draw(triangle);
 		window.display();
 	}
 
